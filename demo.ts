9b52d0932319ed99e226fc5688b67cc292865bc9
Aurpustre <1795904724@qq.com>
2022-08-21 11:53:54 +0800

    Update demo.ts

--- demo.ts
+++ demo.ts
@@ -1,18 +1,28 @@
-function getMaxExp(a: number, m: number, bonus: number[], exp: number[]) {
-    let totalExp = a;
-    for (let i = 0; i < m; i++) {
-        let maxExpIndex = -1;
-        for (let j = 0; j < exp.length; j++) {
-            if (exp[j] < totalExp && (bonus[maxExpIndex] || 0) < bonus[j]) {
-                maxExpIndex = j;
-            }
-        }
-        totalExp += bonus[maxExpIndex];
-        bonus.splice(maxExpIndex, 1);
-        exp.splice(maxExpIndex, 1);
+function superEggDrop(k: number, n: number): number {
+    let dp = Array.from({ length: k + 1 }, () => Array(n + 1).fill(n));
+    for (let i = 0; i <= n; i++) {
+        dp[1][i] = i;
     }
-    return totalExp;
-}
+    for (let i = 0; i <= k; i++) {
+        dp[i][0] = 0;
+        dp[i][1] = 1;
+    }
+    for (let i = 2; i <= k; i++) {
+        for (let j = 2; j <= n; j++) {
+            let start = 2; let end = j;
+            let pos = start;
+            while (start <= end) {
+                let mid = Math.floor((start + end) / 2);                
+                if (dp[i - 1][mid - 1] >= dp[i][j - mid]) {
+                    pos = mid;
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            dp[i][j] = Math.min(dp[i][j], Math.max(dp[i - 1][pos - 1] + 1, dp[i][j - pos] + 1));
+        }
 
-const result = getMaxExp(2, 3, [2, 1, 3, 4], [0, 1, 1, 2]);
-console.log(result);
+    }
+    return dp[k][n];
+};

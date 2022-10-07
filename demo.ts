c37d79900c0bb0434cb3671e54e83d366947ae0b
Aurpustre <1795904724@qq.com>
2022-10-07 23:21:02 +0800

    Update demo.ts

--- demo.ts
+++ demo.ts
@@ -1,52 +1,185 @@
-function superEggDrop(k: number, n: number): number {
-    let dp = Array.from({ length: k + 1 }, () => Array(n + 1).fill(n));
-    for (let i = 0; i <= n; i++) {
-        dp[1][i] = i;
-    }
-    for (let i = 0; i <= k; i++) {
-        dp[i][0] = 0;
-        dp[i][1] = 1;
-    }
-    for (let i = 2; i <= k; i++) {
-        for (let j = 2; j <= n; j++) {
-            let start = 2; let end = j;
-            let pos = start;
-            while (start <= end) {
-                let mid = Math.floor((start + end) / 2);                
-                if (dp[i - 1][mid - 1] >= dp[i][j - mid]) {
-                    pos = mid;
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
+function main(boxList: string[], ballList: any[]) {
+    let result = 0;
+    let startIndex = 1;
+    let lastPaper: number = 0;
+    let numPaper: number = 0;
+    boxList = boxList.sort((a: any, b: any) => a[4] - b[4]);
+
+    function handleData() {
+        let sign = false;
+        const ball0 = ballList[startIndex - 1];
+        const ball1 = ballList[startIndex] || '';
+        const ball2 = ballList[startIndex + 1] || '';
+
+        const dep0 = Number(ball0[2]);
+        const dep1 = Number(ball1[2]);
+        const dep2 = Number(ball2[2]);
+
+        const boxItem = ball0[0] + ball1?.[0] + ball2?.[0];
+        let existBox = boxList.filter(item => item.includes(boxItem));
+
+        if (!sign && existBox.length) {
+            for (let i = 0; i < existBox.length; i++) {
+                const target = existBox[i];
+                const boxPaper = Number(target[4]);
+                const c1 = (boxPaper === 0) && ((dep1 === 0 || dep1 < startIndex) && (dep2 === 0 || dep2 < startIndex));
+                const c2 = (boxPaper === 1) && ((dep1 === 0 || dep1 <= startIndex) && (dep2 === 0 || dep2 <= startIndex));
+                const c3 = (boxPaper === 2) && ((dep1 === 0 || dep1 < startIndex) && (dep2 === 0 || dep2 <= startIndex + 1));
+                if (c1 || c2 || c3) {
+                    if ((lastPaper < dep0 && dep0 < startIndex) || (lastPaper < dep1 && dep1 < startIndex) || (lastPaper < dep2 && dep2 < startIndex)) {
+                        numPaper += 1;
+                        lastPaper = startIndex - 1;
+                    }
+                    if (boxPaper) {
+                        numPaper += 1;
+                        lastPaper = startIndex - 1 + boxPaper;
+                    }
+                    startIndex += 3;
+                    sign = true;
+                    result += 1;
+                    break;
                 }
             }
-            dp[i][j] = Math.min(dp[i][j], Math.max(dp[i - 1][pos - 1] + 1, dp[i][j - pos] + 1));
         }
 
-    }
-    return dp[k][n];
-};
+        if (!sign) {
+            existBox = boxList.filter(item => item[0] === ball0[0] && item[1] === ball1[0]);
+            if (existBox.length) {
+                for (let i = 0; i < existBox.length; i++) {
+                    const target = existBox[i];
+                    const boxPaper = Number(target[4]);
+                    if ((dep1 === 0 || dep1 < startIndex) || (boxPaper === 1 && dep1 <= startIndex)) {
+                        if ((lastPaper < dep0 && dep0 < startIndex) || (lastPaper < dep1 && dep1 < startIndex)) {
+                            numPaper += 1;
+                            lastPaper = startIndex - 1;
+                        }
+                        if (boxPaper) {
+                            numPaper += 1;
+                            lastPaper = startIndex - 1 + boxPaper;
+                        }
+                        startIndex += 2;
+                        sign = true;
+                        result += 1;
+                        break;
+                    }
 
-
-
-function superEggDrop(k: number, n: number): number {
-    let result = 1;
-    let dp = Array.from({ length: k + 1 }, () => Array(0));
-    for (let i = 1; i <= k; i++) {
-        dp[i][1] = 1;
-    }
-    for (let i = 0; i <= n; i++) {
-        dp[1][i] = i;
-    }
-
-    for (let i = 2; i <= n; i++) {
-        for (let j = 2; j <= k; j++) {
-            dp[j][i] = dp[j - 1][i - 1] + dp[j][i - 1] + 1;
+                }
+            }
         }
-        if (dp[k][i] >= n) {
-            result = i;
-            break;
+
+        if (!sign) {
+            existBox = boxList.filter(item => item[1] === ball0[0] && item[2] === ball1[0]);
+            if (existBox.length) {
+                for (let i = 0; i < existBox.length; i++) {
+                    const target = existBox[i];
+                    const boxPaper = Number(target[4]);
+                    if ((dep1 === 0 || dep1 < startIndex) || (boxPaper === 2 && dep1 <= startIndex)) {
+                        if ((lastPaper < dep0 && dep0 < startIndex) || (lastPaper < dep1 && dep1 < startIndex)) {
+                            numPaper += 1;
+                            lastPaper = startIndex - 1;
+                        }
+                        if (boxPaper) {
+                            numPaper += 1;
+                            lastPaper = startIndex - 1 + boxPaper;
+                        }
+                        startIndex += 2;
+                        sign = true;
+                        result += 1;
+                        break;
+                    }
+
+                }
+            }
         }
+
+        if (!sign) {
+            existBox = boxList.filter(item => item[0] === ball0[0] && item[2] === ball1[0]);
+            if (existBox.length) {
+                for (let i = 0; i < existBox.length; i++) {
+                    const target = existBox[i];
+                    const boxPaper = Number(target[4]);
+                    if ((dep1 === 0 || dep1 < startIndex) || ((boxPaper === 1 || boxPaper === 2) && dep1 <= startIndex)) {
+                        if ((lastPaper < dep0 && dep0 < startIndex) || (lastPaper < dep1 && dep1 < startIndex)) {
+                            numPaper += 1;
+                            lastPaper = startIndex - 1;
+                        }
+                        if (boxPaper) {
+                            numPaper += 1;
+                            lastPaper = startIndex - 1 + boxPaper;
+                        }
+                        startIndex += 2;
+                        sign = true;
+                        result += 1;
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        if (!sign) {
+            existBox = boxList.filter(item => item.includes(ball0[0]));
+            if (existBox.length) {
+                for (let i = 0; i < existBox.length; i++) {
+                    const target = existBox[i];
+                    const boxPaper = Number(target[4]);
+                    if (lastPaper < dep0 && dep0 < startIndex) {
+                        numPaper += 1;
+                        lastPaper = startIndex - 1;
+                    }
+                    if (boxPaper) {
+                        numPaper += 1;
+                        lastPaper = startIndex - 1 + boxPaper;
+                    }
+                    startIndex += 1;
+                    sign = true;
+                    result += 1;
+                    break;
+
+                }
+            }
+        }
+        return startIndex;
     }
-    return result;
-};
+
+    while (startIndex <= ballList.length) {
+        handleData();
+    }
+    console.log(result + ' ' + numPaper);
+}
+
+
+process.stdin.setEncoding('utf8');
+const stdin = process.openStdin();
+const buff: any[] = [];
+process.stdin.on('data', (data: any) => {
+    buff.push(data);
+})
+
+process.stdin.once('end', () => {
+    const stdStr = buff.join('').trim();
+    let args: string[] = [];
+    let numBox = 0;
+    let numBall = 0;
+    let boxList: any[] = [];
+    let ballList: any[] = [];
+    try {
+        args = stdStr.split('\n');
+        numBox = Number(args[0].split(' ')[0]);
+        numBall = Number(args[0].split(' ')[1]);
+
+        for (let i = 1; i < numBox + 1; i++) {
+            boxList.push(args[i]);
+        }
+
+        for (let i = 1 + numBox; i < numBall + 1 + numBox; i++) {
+            ballList.push(args[i]);
+        }
+    } catch (error) {
+
+    }
+
+    console.time('solutiuon');
+    main(boxList, ballList);
+    console.timeEnd('solutiuon');
+})
